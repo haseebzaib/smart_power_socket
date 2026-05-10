e534fd63a6ee6a3fa20fe4f1374a4be78ce8d2ef
haseebzaib <hzaib76@gmail.com>
2026-05-10 17:51:08 +0500

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@
 
 hardware::gpioCon utilityPwrLed(GPIO_DT_SPEC_GET(DT_ALIAS(utility_pwr_led), gpios), GPIO_OUTPUT_HIGH);
 hardware::gpioCon cellularLed(GPIO_DT_SPEC_GET(DT_ALIAS(cellular_led), gpios), GPIO_OUTPUT_HIGH);
-hardware::gpioCon gsmPwrKey(GPIO_DT_SPEC_GET(DT_ALIAS(gsm_pwrkey), gpios), GPIO_OUTPUT_HIGH);
+hardware::gpioCon gsmDtr(GPIO_DT_SPEC_GET(DT_ALIAS(gsm_dtr), gpios), GPIO_OUTPUT_INACTIVE);
+hardware::gpioCon gsmPwrKey(GPIO_DT_SPEC_GET(DT_ALIAS(gsm_pwrkey), gpios), GPIO_OUTPUT_INACTIVE);
 hardware::uartCon gsmUart(DEVICE_DT_GET(DT_ALIAS(gsm_uart)));
 sensors::hlw811x energyMeters(
 	DEVICE_DT_GET(DT_ALIAS(hlw_uart)),
@@ -113,10 +114,10 @@ static bool gsmFindBaud(uint32_t &baudrate)
 
 static void gsmPowerPulse()
 {
-	gsmPwrKey.set(0);
-	k_msleep(4000);
 	gsmPwrKey.set(1);
-	k_msleep(6000);
+	k_msleep(4000);
+	gsmPwrKey.set(0);
+	k_msleep(10000);
 }
 
 static void gsmBasicInit()
@@ -156,6 +157,7 @@ int main(void)
 
 	utilityPwrLed.init();
 	cellularLed.init();
+	gsmDtr.init();
 	gsmPwrKey.init();
 	int gsmUartInit = gsmUart.init();
 	std::cout << "GSM UART init: " << gsmUartInit << std::endl;
